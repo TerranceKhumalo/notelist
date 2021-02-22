dda5be4ac9705ce7a708349fc4d23be50a90c9a8
TerranceKhumalo <khumaloterrance@gmail.com>
2021-02-22 20:21:53 +0200

    Delete date.js file

--- date.js
+++
@@ -1,9 +0,0 @@
-module.exports.getDay = function () {
-    const options = {
-        weekday: "long",
-        month: "long",
-        day: "numeric"
-    }
-    const today = new Date();
-    return today.toLocaleDateString("en-US", options);
-}
